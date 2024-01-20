0e0fbab12b341a0b0271d3d5f03c5a4b205a8bee
kwon <kyjyj6334@gmail.com>
2024-01-20 18:32:18 +0900

    HTTP 요청 메시지 - JSON

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -56,5 +56,23 @@ public class RequestBodyStringController {
         return "ok";
     }
 
+    /**
+     * @RequestBody
+        @RequestBody 를 사용하면 HTTP 메시지 바디 정보를 편리하게 조회할 수 있다. 참고로 헤더 정보가 필요하다면
+        HttpEntity 를 사용하거나 @RequestHeader 를 사용하면 된다.
+        이렇게 메시지 바디를 직접 조회하는 기능은 요청 파라미터를 조회하는 @RequestParam , @ModelAttribute 와
+        는 전혀 관계가 없다.
+
+        
+        요청 파라미터 vs HTTP 메시지 바디
+        요청 파라미터를 조회하는 기능: @RequestParam , @ModelAttribute
+        HTTP 메시지 바디를 직접 조회하는 기능: @RequestBody
+
+
+        @ResponseBody
+        @ResponseBody 를 사용하면 응답 결과를 HTTP 메시지 바디에 직접 담아서 전달할 수 있다.
+        물론 이 경우에도 view를 사용하지 않는다.
+     */
+
     
 }
